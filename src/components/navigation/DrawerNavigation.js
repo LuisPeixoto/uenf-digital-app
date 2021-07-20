8aa1f9d085b6d3d59f8169538342abff8e538d33
LuisPeixoto <luispeixoto@pq.uenf.br>
2021-07-20 10:08:37 -0300

    Atribuido uma key para os itens da lista

--- src/components/navigation/DrawerNavigation.js
+++ src/components/navigation/DrawerNavigation.js
@@ -1,78 +1,95 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
-import { Container, Icon, Header, Footer, Content } from 'native-base'
-import { DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
-import { items } from './itemsDrawerNavigation';
-function Sidebar({ ...props }) {
+import {View, StyleSheet, Image} from 'react-native';
+import {Container, Icon, Header, Footer, Content} from 'native-base';
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from '@react-navigation/drawer';
+import {items} from './itemsDrawerNavigation';
+function Sidebar({...props}) {
   return (
     <Container>
-      <Header style={{ backgroundColor: '#7ebf83', borderBottomWidth: 0, height: 100 }} androidStatusBarColor="#7EBF83" iosBarStyle="#7EBF83">
+      <Header
+        style={styles.header}
+        androidStatusBarColor="#7EBF83"
+        iosBarStyle="#7EBF83">
         <View style={styles.logoHeader}>
-          <Image source={require('../../assets/logos/logo.png')} style={styles.logo} />
+          <Image
+            source={require('../../assets/logos/logo.png')}
+            style={styles.logo}
+          />
         </View>
-
       </Header>
 
       <Content>
-        <DrawerContentScrollView {...props} >
+        <DrawerContentScrollView {...props}>
           <DrawerItemList {...props} />
 
-          {items.map((item) => {
+          {items.map((item, index) => {
             return (
-              <DrawerItem label={item.title} icon={({ color, size }) => (
-                <Icon
-                  name={item.nameIcon}
-                  type={item.typeIcon}
-                  solid={true}
-                  style={{
-                    fontSize: size,
-                    color: color,
-                    textAlign: "center",
-                    width: 30
-                  }} />
-              )}
+              <DrawerItem
+                key={index}
+                label={item.title}
+                icon={({color, size}) => (
+                  <Icon
+                    name={item.nameIcon}
+                    type={item.typeIcon}
+                    solid={true}
+                    style={{
+                      fontSize: size,
+                      color: color,
+                      textAlign: 'center',
+                      width: 30,
+                    }}
+                  />
+                )}
                 onPress={() =>
                   props.navigation.navigate('Início', {
                     screen: item.screen,
-                    params: { url: item.url },
+                    params: {url: item.url},
                   })
-                } />
-            )
+                }
+              />
+            );
           })}
         </DrawerContentScrollView>
       </Content>
-      <Footer style={{ backgroundColor: '#ffffff' }}>
-
-
+      <Footer style={{backgroundColor: '#ffffff'}}>
         <DrawerContentScrollView>
-          <DrawerItem label="Sobre nós"
+          <DrawerItem
+            label="Sobre nós"
             onPress={() =>
               props.navigation.navigate('Início', {
                 screen: 'About',
               })
-            } />
-
+            }
+          />
         </DrawerContentScrollView>
       </Footer>
     </Container>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   logoHeader: {
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: '#7ebf83',
+    borderBottomWidth: 0,
+    height: 100,
   },
 
   logo: {
     width: 130,
     height: 57,
-    alignItems: "center",
-    justifyContent: "center",
-
-  }
-})
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default Sidebar;
