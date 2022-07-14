38601804b7f4a1df4167934eb9a769318b25f77a
luispeixoto <luispeixoto@pq.uenf.br>
2022-07-14 13:24:34 -0300

    feat: component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,21 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
 import { Container, Logo, Menu, Right, Search, Bell, Button } from './styles';
 
 export function Header() {
+  const navigation = useNavigation<any>();
   return (
     <Container>
-      <Button>
+      <Button onPress={() => navigation.toggleDrawer()}>
         <Menu />
       </Button>
-
       <Logo />
-
       <Right>
-        <Button>
+        <Button onPress={() => navigation.navigate('Search')}>
           <Search />
         </Button>
 
-        <Button>
+        <Button onPress={() => navigation.navigate('Notifications')}>
           <Bell />
         </Button>
       </Right>
